9551aa0eb591d9947f1417eec454c947636b0d3b
qidate001 <qidate001@gmail.com>
2026-08-22 16:52:36 +0800

    feat(鬼书): 为鬼书添加了LLM对话

--- src/main/java/com/qidate/qisplan2/client/GhostBookClient.java
+++ src/main/java/com/qidate/qisplan2/client/GhostBookClient.java
@@ -7,9 +7,15 @@ public final class GhostBookClient {
     private GhostBookClient() {
     }
 
-    public static void open() {
+    public static void open(
+            String apiKey,
+            String modelName
+    ) {
         Minecraft.getInstance().setScreen(
-                new GhostBookScreen()
+                new GhostBookScreen(
+                        apiKey,
+                        modelName
+                )
         );
     }
 }
