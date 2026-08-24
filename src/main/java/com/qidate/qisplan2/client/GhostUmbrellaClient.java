537282c23b1716090f8d97a2cee1bef207711276
qidate001 <qidate001@gmail.com>
2026-08-24 17:38:05 +0800

    feat(鬼雨伞): 初步添加能够大概显示模型的版本

--- src/main/java/com/qidate/qisplan2/client/GhostUmbrellaClient.java
+++ src/main/java/com/qidate/qisplan2/client/GhostUmbrellaClient.java
@@ -1,21 +1,23 @@
 package com.qidate.qisplan2.client;
 
 import com.qidate.qisplan2.QisPlan2;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.ModelEvent;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
+import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 
 public final class GhostUmbrellaClient {
 
     private GhostUmbrellaClient() {
     }
 
+    @SubscribeEvent
     public static void registerAdditionalModels(
             ModelEvent.RegisterAdditional event
     ) {
-        /*
-         * 关闭状态模型
-         */
         event.register(
                 ModelResourceLocation.standalone(
                         ResourceLocation.fromNamespaceAndPath(
@@ -25,9 +27,6 @@ public final class GhostUmbrellaClient {
                 )
         );
 
-        /*
-         * 打开状态模型
-         */
         event.register(
                 ModelResourceLocation.standalone(
                         ResourceLocation.fromNamespaceAndPath(
@@ -35,6 +34,29 @@ public final class GhostUmbrellaClient {
                                 "item/ghost_umbrella_open"
                         )
                 )
+        );
+    }
+
+    @SubscribeEvent
+    public static void registerClientExtensions(
+            RegisterClientExtensionsEvent event
+    ) {
+        QisPlan2.LOGGER.info(
+                "[QisPlan2] 注册鬼雨伞客户端渲染器"
+        );
+
+        event.registerItem(
+                new IClientItemExtensions() {
+
+                    private final GhostUmbrellaRenderer renderer =
+                            new GhostUmbrellaRenderer();
+
+                    @Override
+                    public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                        return renderer;
+                    }
+                },
+                QisPlan2.GHOST_UMBRELLA.get()
         );
     }
 }
